2d82ad269ea8a618e6a5c2edf8fb2801e77f9ff1
Jakob Borg <jakob@nym.se>
2009-12-11 22:34:46 +0100

    Remove nonfunctional functional tests

--- test/functional/graduations_controller_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class GraduationsControllerTest < ActionController::TestCase
-end
--- test/functional/payments_controller_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class PaymentsControllerTest < ActionController::TestCase
-end
