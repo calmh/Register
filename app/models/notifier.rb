a851f8305f889ce07fb30d7c8b8c101ce67bdb51
www-data <www-data@yat.fiveforms.org>
2010-01-26 17:32:31 +0100

    Fix from address

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -3,7 +3,7 @@ class Notifier < ActionMailer::Base
 
   def password_reset_instructions(user)
     subject       "Password Reset Instructions"
-    from          "TIA Register <register@fiveforms.org>"
+    from          "webmaster@fiveforms.org"
     recipients    user.email
     sent_on       Time.now
     body          :edit_password_reset_url => edit_password_reset_url(user.perishable_token)
